adafafe4a54096b25e70857b05f783d9f0db8b04
bluetechnology <temtrirat@gmail.com>
2016-04-02 15:06:32 +0700

    Finish 2April16

--- app/src/main/java/appme/bluetech/learntraffic/MainActivity.java
+++ app/src/main/java/appme/bluetech/learntraffic/MainActivity.java
@@ -1,13 +1,39 @@
 package appme.bluetech.learntraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit การประกาศตัวแปร เพื่อบ่งบอกว่ามือถือมีแรมพอไหม?
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget คือการผูกความสัมพันธ์ของตัวแปร (Variable and Widget)
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        //การเก็บเหตุการณ์ จากการคลิกปุ่ม
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                mediaPlayer.start();
+            } // onClick
+        });
+
+    }   // Main Method นี่คือ เมธอด
+
+}   // Main Class นี่คือคลาสหลัก
